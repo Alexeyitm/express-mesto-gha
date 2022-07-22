64405884b723851be66d102dcf56c0fff1acd488
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-22 18:42:04 +0500

    feat: add controllers

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const bodyParser = require('body-parser');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {});
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log('Example app listening on port 3000!');
 });
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const User = require('../models/user');
+
+router.get('/', (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+router.get('/:userId', (req, res) => {
+  User.findById(req.params.id)
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+router.post('/', (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+module.exports = router;
