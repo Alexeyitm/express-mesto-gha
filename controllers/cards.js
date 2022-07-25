1d0a7dca53564fda56c231b7f9d405ac51ab5f28
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-25 11:31:49 +0500

    ref: ref code

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,47 +28,55 @@ module.exports.deleteCardById = (req, res) => {
       }
       res.status(404).send({ message: 'Карточка с указанным id не найдена.' });
     })
-    .catch(() => {
-      res.status(500).send({ message: 'Ошибка сервера' });
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Передан некорректный id карточки' });
+        return;
+      }
+      res.status(500).send({ message: 'Ошибка сервера.' });
     });
 };
 
-module.exports.addLike = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } },
-  { new: true },
-)
-  .then((user) => {
-    if (user) {
-      res.send({ data: user });
-      return;
-    }
-    res.status(404).send({ message: 'Передан несуществующий id карточки.' });
-  })
-  .catch((err) => {
-    if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
-      return;
-    }
-    res.status(500).send({ message: 'Ошибка сервера.' });
-  });
+module.exports.addLike = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+        return;
+      }
+      res.status(404).send({ message: 'Передан несуществующий id карточки.' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+        return;
+      }
+      res.status(500).send({ message: 'Ошибка сервера.' });
+    });
+};
 
-module.exports.deleteLike = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } },
-  { new: true },
-)
-  .then((user) => {
-    if (user) {
-      res.send({ data: user });
-      return;
-    }
-    res.status(404).send({ message: 'Передан несуществующий id карточки.' });
-  })
-  .catch((err) => {
-    if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
-      return;
-    }
-    res.status(500).send({ message: 'Ошибка сервера.' });
-  });
+module.exports.deleteLike = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+        return;
+      }
+      res.status(404).send({ message: 'Передан несуществующий id карточки.' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+        return;
+      }
+      res.status(500).send({ message: 'Ошибка сервера.' });
+    });
+};
