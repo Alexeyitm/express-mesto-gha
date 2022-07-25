c501a53f63b63fc2c4276c264b7dc7c690ee7d74
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-25 11:08:03 +0500

    feat: add 404

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {});
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use('/*', (req, res) => res.status(404).send('Страница не найдена'));
+
 app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
 });
