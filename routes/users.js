75a86392e284be9497a067b0c6443a30f4ba68b7
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-03 12:55:05 +0500

    fix: add unknown

--- routes/users.js
+++ routes/users.js
@@ -13,12 +13,12 @@ router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-  }),
+  }).unknown(true),
 }), updateUser);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string(),
-  }),
+  }).unknown(true),
 }), updateAvatar);
 
 module.exports = router;
