7e80e642375856a3b7616e96d201c946bce50648
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-03 12:04:03 +0500

    fix: delete send

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,6 @@ module.exports.login = (req, res) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.send({ token });
       res
         .cookie('jwt', token, {
           maxAge: '7d',
