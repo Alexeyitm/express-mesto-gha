a2e0c3d51fd0e1fd3bb19f13dc4c3148bab0f5c7
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-07 11:06:10 +0500

    fix: fixed joi for signup

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().link(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }).unknown(true),
