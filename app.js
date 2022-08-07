08762a2403864051b1cc5f3bc346b73d80920d0d
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-07 11:13:11 +0500

    fix: fixed bag

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().link(),
+    avatar: Joi.link(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }).unknown(true),
