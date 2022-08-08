452f8a02a8a6a7fcd359f587305c42a9bdba0cca
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-08 10:54:26 +0500

    fix: delete joi link

--- routes/cards.js
+++ routes/cards.js
@@ -12,7 +12,6 @@ router.get('/', getAllCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().regex(/http(s)?:\/\/(www.)?[/w]/),
   }).unknown(true),
 }), createCard);
 router.delete('/:cardId', deleteCardById);
