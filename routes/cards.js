9dd3c80a0c9d0fe1ee366a40f88f42c7f75c6e3e
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-08 16:19:55 +0500

    feat: add regex for link

--- routes/cards.js
+++ routes/cards.js
@@ -13,6 +13,7 @@ router.get('/', getAllCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/^http(s)?:\/\/(www.)?([\w-]+\.)+\/?\S*$/),
   }).unknown(true),
 }), createCard);
 
