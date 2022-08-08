b4ceb84d1c3729186a1ed62101f511e323d489a6
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-08 10:31:28 +0500

    feat: add regex

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getAllCards,
   createCard,
@@ -8,7 +9,12 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getAllCards);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/http(s)?:\/\/(www.)?[/w]/),
+  }).unknown(true),
+}), createCard);
 router.delete('/:cardId', deleteCardById);
 router.put('/:cardId/likes', addLike);
 router.delete('/:cardId/likes', deleteLike);
