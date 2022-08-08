fe6f1e0d39540fe2bad295eeb7d7589315d4322e
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-08 16:21:13 +0500

    feat: add regex for avatar

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
+    avatar: Joi.string().required().regex(/^http(s)?:\/\/(www.)?([\w-]+\.)+\/?\S*$/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }).unknown(true),
